e1d95cd91796c06e027a166307df890207966284
David Gong <davidgonghongshen@gmail.com>
2022-11-15 09:08:46 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
-//<editor-fold desc="File Description">
-// DESCRIPTION :
-//       This file serves as the entry point of the program where the execution begins
-//
-//  PUBLIC FUNCTIONS :
-//       None
-//
-//  NOTES :
-//       None
-//
-//  AUTHOR :    B1146 DAVID GONG
-//
-//  CHANGES :
-//       None
-//</editor-fold>
-
 import repository.*;
-import service.ClassVenuesService;
-import service.StudentResultService;
-import service.StudentService;
+import service.*;
 
 import java.util.Scanner;
 
